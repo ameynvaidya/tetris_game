732176eb75157b1dd9f5911d4734dd3a0782a5e5
Amey Vaidya <ameynvaidya@gmail.com>
2020-09-30 21:13:59 -0700

    [6/x] Initial changes to move a piece

--- tetris.py
+++ tetris.py
@@ -17,12 +17,14 @@ class TetrisGame:
     def __init__(self, width=10, height=20):
         self._running = True
         self._display_surf = None
+        self.__piece_surf = None
+        self._clock = pygame.time.Clock()
         self._width = width
         self._height = height
         self.display_size = SCREEN_WIDTH, SCREEN_HEIGHT
         self._board = board.Board(width, height)
-        # # set some random blocks on board
-        # self._board.fill_fake_data()
+        # set some random blocks on board
+        self._board.fill_fake_data()
 
     def on_init(self):
         pygame.init()
@@ -31,29 +33,31 @@ class TetrisGame:
         pygame.display.set_caption("Tetris")
         self._board_surf = ui_board.BoardSprite(
             self._board, SCREEN_WIDTH, SCREEN_HEIGHT)
-
         self._piece_surf = ui_piece.PieceSprite(
             self._board_surf.board_cell_width, 
-            piece.Piece.get_pieces()[5])
+            piece.Piece.get_pieces()[1])
+        
+        # Initialize the location of the board and the piece to be in the 
+        # center of the screen
+        board_left_top_x = (SCREEN_WIDTH - self._board_surf.width()) / 2
+        board_left_top_y = (SCREEN_HEIGHT - self._board_surf.height()) / 2
+
+        self._board_surf.rect.move_ip((board_left_top_x, board_left_top_y))
+        self._piece_surf.rect.move_ip((board_left_top_x, board_left_top_y + self._board_surf.board_cell_width))
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
 
     def on_loop(self):
+        self._piece_surf.rect.move_ip(0, self._board_surf.board_cell_width)
         pass
 
     def on_render(self):
-        board_left_top_x = (SCREEN_WIDTH - self._board_surf.width()) / 2
-        board_left_top_y = (SCREEN_HEIGHT - self._board_surf.height()) / 2
-
-        self._display_surf.blit(
-            self._board_surf.surf,
-            (board_left_top_x, board_left_top_y))
-        self._display_surf.blit(
-            self._piece_surf.surf, 
-            (board_left_top_x, board_left_top_y))
+        self._display_surf.blit(self._board_surf.surf, self._board_surf.rect)
+        self._display_surf.blit(self._piece_surf.surf, self._piece_surf.rect)
         pygame.display.flip()
+        self._clock.tick(20)
 
     def on_cleanup(self):
         pygame.quit()
--- ui/piece.py
+++ ui/piece.py
@@ -5,7 +5,8 @@ import lib.point as pt
 class PieceSprite(pygame.sprite.Sprite):
     def __init__(self, cell_width: int, piece: pc.Piece):
         super(PieceSprite, self).__init__()
-        self.surf = pygame.Surface((cell_width * 4, cell_width * 4), pygame.SRCALPHA)
+        self.piece_width = 4
+        self.surf = pygame.Surface((cell_width * self.piece_width, cell_width * self.piece_width), pygame.SRCALPHA)
         self.rect = self.surf.get_rect()
         for point in piece.getBody():
             point_left = self.rect.left + point.getX() * cell_width
@@ -15,5 +16,21 @@ class PieceSprite(pygame.sprite.Sprite):
                 pc.Piece.piece_color_map()[piece.getColor()],
                 (point_left, point_top, cell_width, cell_width)
             )
+        # Render border for the piece
+        for i in range(self.piece_width + 2):
+            ref_line_x = self.rect.left + i * cell_width - 1
+            pygame.draw.line(
+                self.surf,
+                (0, 0, 0),
+                (ref_line_x, self.rect.top),
+                (ref_line_x, self.rect.bottom), 2)
+
+        for i in range(self.piece_width + 2):
+            ref_line_y = self.rect.top + i * cell_width - 1
+            pygame.draw.line(
+                self.surf,
+                (0, 0, 0),
+                (self.rect.left, ref_line_y),
+                (self.rect.right, ref_line_y), 2)
 
 
